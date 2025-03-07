ad37b25bfd5c94cd4d3548c1540b95f6b822adaf
Gavin Hensley <gavinhensley@protonmail.com>
2025-03-07 00:02:26 -0500

    Add files

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import NavBar from './Nav';
+import NoteContainer from "./Notes";
+
+function App() {
+
+  return (
+    <>
+      <NavBar />
+      <NoteContainer />
+    </>
+  )
+
+}
+
+export default App
---
+++ src/components/Nav.jsx
@@ -0,0 +1,9 @@
+import "../styles/Nav.css"
+
+export default function NavBar () {
+    return (
+        <nav>
+            <h1 className="nav-title">Note Buddy</h1>
+        </nav>
+    )
+}
